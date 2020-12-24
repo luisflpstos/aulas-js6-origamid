8b318de2428715935f84ac85e4cab16dc4183431
Felipe Santos <dev@luisfelipesantos.com.br>
2020-12-24 12:13:37 -0300

    continuando aulas js

---
+++ Objetos/script.js
@@ -0,0 +1,29 @@
+// var pessoa = {
+//   nome: 'Felipe',
+//   idade: 30,
+// }
+
+// var quadrado = {
+//   lados: 4,
+//   area: function (lado) {
+//     return lado * lado;
+//   },
+
+//   perimetro: function (lado) {
+//     return this.lados * lado;
+//   }
+// }
+
+// console.log(quadrado.area(5));
+// console.log(quadrado.perimetro(5));
+
+// nomeie 3 propriedades ou métodos strings
+
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+// busque na web um objeto (método) capaz de interagir com o Clipboard
+
+
+navigator.clipboard.writeText();
