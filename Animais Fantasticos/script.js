730dba5b14ef886c3878db46a2f4628e0119cc6f
Felipe Santos <dev@luisfelipesantos.com.br>
2021-01-07 22:49:15 -0300

    add exercicios dom - 01

--- Animais Fantasticos/script.js
+++ Animais Fantasticos/script.js
@@ -1,47 +1,22 @@
-// TIPOS DE VARIÁVEIS
-// string = "Felipe"
-// Number = 1
-// Boolean = true/false
-// undefined = undefined
-// null = null
-// Symbol = 
-// object = 
+// Retorne no console todas as imagens do site
 
-// var nome = "Andre";
-// var idade = 28;
-// var simbolo = Symbol();
-// console.log(typeof idade);
+const imgsSite = document.getElementsByTagName('img');
+console.log(imgsSite);
 
-// var nome = 'Felipe';
-// var sobrenome = 'Santos';
-// var nomeCompleto = `${nome} ${sobrenome}`;
+// Retorne no console apenas as imagens que começaram com a palavra imagem
 
-// console.log(nomeCompleto);
+const imgNameImagem = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imgNameImagem)
+
+// Selecione todos os links interno (onde o href começa com #)
+const selectHref = document.querySelectorAll('a[href^="#"]');
+console.log(selectHref)
 
 
-//  Declare uma variável contendo uma String
+// Selecione o primeiro h2 dentro de .animais-descricao
+const selectH2 = document.querySelector('h2');
+console.log(selectH2)
 
-// var nome = 'Felipe';
-
-// //  Declare uma variavel contendo um numero dentro de uma String
-
-// // var peso = '90kg';
-
-// //  Declare uma variavel com a sua idade
-
-// var idade = 29;
-
-// //  Declare duas variaveis, uma com seu nome e outra com seu sobrenome e some as mesmas
-
-// var sobrenome = 'Santos';
-// var nomeCompleto = `${nome} ${sobrenome}`;
-// console.log(nomeCompleto);
-
-// // Coloque a seguinte frase em uma variavel: It's time
-
-// var time = "It's time";
+// Selecione o último p do site
 
 
-// // Verifique o tipo de variavel que contem seu nome
-
-// console.log(typeof nome);
