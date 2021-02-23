67b62794400375fbecfad8f9a95682d1f7a528f6
Felipe Santos <dev@luisfelipesantos.com.br>
2021-02-23 19:53:23 -0300

    add aulas JS

--- Animais Fantasticos/script.js
+++ Animais Fantasticos/script.js
@@ -22,8 +22,70 @@
 // const selectLastP = document.querySelectorAll('p');
 // console.log(selectLastP[6]);
 
-const imgs = document.querySelectorAll('img');
+// Mostre no console cada parágrafo do site
 
-imgs.forEach (function(item, index, array) {
-  console.log(item, index, array)
-});
+// const paragrafos = document.querySelectorAll('p');
+// console.log(paragrafos);
+
+// paragrafos.forEach((item) => {
+//   console.log(item);
+// });
+
+// // Mostre o texto dos parágrafos no console
+
+// paragrafos.forEach ((item) => {
+//   console.log(item.innerText);
+// });
+
+// // Como corrigir os erros abaixo:
+
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach((item, index) => {
+//   console.log(item, index);
+// });
+
+// let i = 0;
+
+// imgs.forEach(() => {console.log(i++)});
+
+// imgs.forEach(() => i++);
+
+// const menu = document.querySelector('.menu');
+
+// menu.classList.add('ativo');
+// menu.classList.remove('azul');
+// menu.classList.toggle('azul'); 
+
+
+
+
+// Adicione a classe ativo a todos os itens do menu
+
+// const menuAtivos = document.querySelectorAll('.menu a');
+// menuAtivos.forEach((item) => {
+//     item.classList.add('ativo');
+//   });
+
+// // // Remova a classe ativo de todos os itens do menu e matenha apenas no primeiro
+// // menuAtivos.forEach((item) => {
+// //   item.classList.remove('ativo');
+// // });
+
+// // menuAtivos[0].classList.add('ativo');
+
+// // // Verifique se as imagens possuem o atributo alt
+
+// // const altImgs = document.querySelectorAll('img');
+
+// // altImgs.forEach((img) => {
+// //   const possuiAlt = img.hasAttribute('alt');
+// //   console.log(img, possuiAlt);
+// // });
+
+// // Modifique o href do link esterno do menu
+
+// const link = document.querySelector('a[href^="https"]');
+// link.setAttribute('href', 'https://www.google.com');
+
+
