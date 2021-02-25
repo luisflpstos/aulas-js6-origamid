481c41a5d6feae7c79888c3ea1bfb505e2868b97
Felipe Santos <dev@luisfelipesantos.com.br>
2021-02-24 23:36:34 -0300

    add dom classes

--- Animais Fantasticos/script.js
+++ Animais Fantasticos/script.js
@@ -89,3 +89,30 @@
 // link.setAttribute('href', 'https://www.google.com');
 
 
+
+
+
+// Verifique a distância da primeira imagem em relação ao topo da página
+
+const firstImg = document.querySelector('img');
+const distImg = firstImg.offsetTop;
+ 
+console.log(distImg);
+
+// Retorne a soma da largura de todas as Imagens
+
+const allImgs = document.querySelectorAll('img');
+
+let soma = 0;
+
+allImgs.forEach((allImgs) => {
+  soma += allImgs.naturalWidth;
+  console.log(soma);
+});
+
+// Verifique se os links da página possuem o mínimo recomendado para telas utilizadas com o dedo. (48px/48px de acordo com o google)
+
+const links = document.querySelectorAll('href');
+console.log(links);
+
+// Se o browser for menor que 720px adicione a classe menu-mobile ao menu
